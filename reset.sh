443c24a302a9a35035457948a661944f4e46ab22
Yun Huang Yong <goosmurf@users.noreply.github.com>
2026-04-21 17:48:13 +1000

    reset more things

--- reset.sh
+++ reset.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+kubectl rollout restart -n istio-system daemonset/ztunnel
 kubectl rollout restart -n istio-gateway deployment/istio-gateway-istio
 kubectl rollout restart -n istio-waypoint deployment/istio-waypoint
 kubectl rollout restart -n istio-system deployment/prometheus
